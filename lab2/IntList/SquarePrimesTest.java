c96743374e1cb1768392cf3afec8573ab4b41171
Str Bytes <strbytes@gmail.com>
2022-07-16 08:23:40 -0400

    Finish lab2 exercises

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -16,5 +16,17 @@ public class SquarePrimesTest {
         boolean changed = IntListExercises.squarePrimes(lst);
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
+        lst = IntList.of(14, 15, 16, 18, 17);
+        changed = IntListExercises.squarePrimes(lst);
+        assertEquals("14 -> 15 -> 16 -> 18 -> 289", lst.toString());
+        assertTrue(changed);
+        lst = IntList.of(14, 15, 16, 18, 20);
+        changed = IntListExercises.squarePrimes(lst);
+        assertEquals("14 -> 15 -> 16 -> 18 -> 20", lst.toString());
+        assertFalse(changed);
+        lst = IntList.of(2, 3, 4, 5);
+        changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 9 -> 4 -> 25", lst.toString());
+        assertTrue(changed);
     }
 }
