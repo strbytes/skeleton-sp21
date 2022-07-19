dfd3e1ec70d20ce10b8fced06ed31a93413f73b4
Str Bytes <strbytes@gmail.com>
2022-07-19 08:40:50 -0400

    Add javadocs and fix style issues

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -2,6 +2,7 @@ package deque;
 
 /**
  * Array implementation of a deque.
+ * @author Str Bytes
  * @param <T> Type of data to be stored in the deque.
  */
 public class ArrayDeque<T> {
@@ -14,7 +15,7 @@ public class ArrayDeque<T> {
     /** Index of the last item in the list. */
     private int last;
 
-    /** Constructs an empty ArrayDeque */
+    /** Constructs an empty ArrayDeque. */
     public ArrayDeque() {
         items = (T[]) new Object[8];
         size = 0;
@@ -22,6 +23,10 @@ public class ArrayDeque<T> {
         last = 4;
     }
 
+    /** Resize the array used to implement the deque.
+     *
+     * @param len The desired length of the new array.
+     */
     private void resize(int len) {
         T[] a = (T[]) new Object[len];
         System.arraycopy(items, 0, a, 0, last);
@@ -30,15 +35,23 @@ public class ArrayDeque<T> {
         items = a;
     }
 
+    /** Adds an item to the beginning of the deque.
+     *
+     * @param item The item to be added to the deque.
+     */
     public void addFirst(T item) {
         if (size == items.length) {
             resize(items.length * 2);
         }
         items[first] = item;
         size += 1;
-        first = Math.floorMod((first - 1),items.length);
+        first = Math.floorMod((first - 1), items.length);
     }
 
+    /** Adds an item to the end of the deque.
+     *
+     * @param item The item to be added to the deque.
+     */
     public void addLast(T item) {
         if (size == items.length) {
             resize(items.length * 2);
@@ -48,23 +61,32 @@ public class ArrayDeque<T> {
         last = Math.floorMod((last + 1), items.length);
     }
 
+    /** Returns a value in the deque by index.
+     *
+     * @param i The index of the value to be retrieved.
+     * @return Returns the value held at index i of the deque.
+     */
     public T get(int i) {
         assert (i < size);
         return items[(first + 1 + i) % items.length];
     }
 
+    /** Returns whether the deque is empty or not. */
     public boolean isEmpty() {
         return size == 0;
     }
 
+    /** Print a string representation of the deque. */
     public void printDeque() {
         System.out.println(this.toString());
     }
 
+    /** Return the size of the deque. */
     public int size() {
         return size;
     }
 
+    /** Return a String representation of the deque. */
     public String toString() {
         String str = "";
         for (int i = 0; i < size; i++) {
@@ -76,6 +98,10 @@ public class ArrayDeque<T> {
         return str;
     }
 
+    /** Remove the first item from the deque and return its value.
+     *
+     * @return The value of the item removed from the deque.
+     */
     public T removeFirst() {
         if (size <= items.length * 0.25 && items.length > 8) {
             resize(items.length / 2);
@@ -87,6 +113,10 @@ public class ArrayDeque<T> {
         return val;
     }
 
+    /** Remove the last item from the deque and return its value.
+     *
+     * @return The value of the item removed from the deque.
+     */
     public T removeLast() {
         if (size <= items.length * 0.25 && items.length > 8) {
             resize(items.length / 2);
