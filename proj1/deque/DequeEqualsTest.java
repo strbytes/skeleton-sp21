2fc352308df086f1cfa1d1723731e54786ff071d
Str Bytes <strbytes@gmail.com>
2022-07-30 08:49:52 -0400

    Fix style in DequeEqualsTest again

--- proj1/deque/DequeEqualsTest.java
+++ proj1/deque/DequeEqualsTest.java
@@ -1,46 +1,47 @@
 package deque;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DequeEqualsTest {
     @Test
-    public void LinkedListDequeEqualsTest() {
-        LinkedListDeque<Integer> LinkedListDeque1 = LinkedListDeque.of(0, 1, 2);
-        LinkedListDeque<Integer> LinkedListDeque2 = LinkedListDeque.of(0, 1, 2);
-        LinkedListDeque<Integer> LinkedListDeque3 = LinkedListDeque.of(1, 1, 2);
-        LinkedListDeque<String> LinkedListDeque4 = LinkedListDeque.of("one", "two", "three");
-        LinkedListDeque<String> LinkedListDeque5 = LinkedListDeque.of("one", "two", "three");
+    public void linkedListDequeEqualsTest() {
+        LinkedListDeque<Integer> linkedListDeque1 = LinkedListDeque.of(0, 1, 2);
+        LinkedListDeque<Integer> linkedListDeque2 = LinkedListDeque.of(0, 1, 2);
+        LinkedListDeque<Integer> linkedListDeque3 = LinkedListDeque.of(1, 1, 2);
+        LinkedListDeque<String> linkedListDeque4 = LinkedListDeque.of("one", "two", "three");
+        LinkedListDeque<String> linkedListDeque5 = LinkedListDeque.of("one", "two", "three");
 
-        assertEquals(LinkedListDeque4, LinkedListDeque5);
-        assertEquals(LinkedListDeque1, LinkedListDeque2);
-        assertNotEquals(LinkedListDeque1, LinkedListDeque3);
-        assertNotEquals(LinkedListDeque2, LinkedListDeque4);
+        assertEquals(linkedListDeque4, linkedListDeque5);
+        assertEquals(linkedListDeque1, linkedListDeque2);
+        assertNotEquals(linkedListDeque1, linkedListDeque3);
+        assertNotEquals(linkedListDeque2, linkedListDeque4);
     }
 
     @Test
-    public void ArrayDequeEqualsTest() {
-        ArrayDeque<Integer> ArrayDeque1 = ArrayDeque.of(0, 1, 2);
-        ArrayDeque<Integer> ArrayDeque2 = ArrayDeque.of(0, 1, 2);
-        ArrayDeque<Integer> ArrayDeque3 = ArrayDeque.of(1, 1, 2);
-        ArrayDeque<String> ArrayDeque4 = ArrayDeque.of("one", "two", "three");
-        ArrayDeque<String> ArrayDeque5 = ArrayDeque.of("one", "two", "three");
+    public void arrayDequeEqualsTest() {
+        ArrayDeque<Integer> arrayDeque1 = ArrayDeque.of(0, 1, 2);
+        ArrayDeque<Integer> arrayDeque2 = ArrayDeque.of(0, 1, 2);
+        ArrayDeque<Integer> arrayDeque3 = ArrayDeque.of(1, 1, 2);
+        ArrayDeque<String> arrayDeque4 = ArrayDeque.of("one", "two", "three");
+        ArrayDeque<String> arrayDeque5 = ArrayDeque.of("one", "two", "three");
 
-        assertEquals(ArrayDeque1, ArrayDeque2);
-        assertEquals(ArrayDeque4, ArrayDeque5);
-        assertNotEquals(ArrayDeque1, ArrayDeque3);
-        assertNotEquals(ArrayDeque2, ArrayDeque4);
+        assertEquals(arrayDeque1, arrayDeque2);
+        assertEquals(arrayDeque4, arrayDeque5);
+        assertNotEquals(arrayDeque1, arrayDeque3);
+        assertNotEquals(arrayDeque2, arrayDeque4);
     }
 
     @Test
-    public void CrossTypeTest() {
-        LinkedListDeque<Integer> LinkedListDeque1 = LinkedListDeque.of(0, 1, 2);
-        LinkedListDeque<Integer> LinkedListDeque2 = LinkedListDeque.of(1, 1, 2);
-        ArrayDeque<Integer> ArrayDeque1 = ArrayDeque.of(0, 1, 2);
-        ArrayDeque<String> ArrayDeque2 = ArrayDeque.of("one", "two", "three");
+    public void crossTypeTest() {
+        LinkedListDeque<Integer> linkedListDeque1 = LinkedListDeque.of(0, 1, 2);
+        LinkedListDeque<Integer> linkedListDeque2 = LinkedListDeque.of(1, 1, 2);
+        ArrayDeque<Integer> arrayDeque1 = ArrayDeque.of(0, 1, 2);
+        ArrayDeque<String> arrayDeque2 = ArrayDeque.of("one", "two", "three");
 
-        assertEquals(LinkedListDeque1, ArrayDeque1);
-        assertNotEquals(LinkedListDeque2, ArrayDeque1);
-        assertNotEquals(LinkedListDeque1, ArrayDeque2);
+        assertEquals(linkedListDeque1, arrayDeque1);
+        assertNotEquals(linkedListDeque2, arrayDeque1);
+        assertNotEquals(linkedListDeque1, arrayDeque2);
     }
 }
