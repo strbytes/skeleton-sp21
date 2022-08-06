60cba6893d60802c1328f9c7923b538dd0824e9a
Str Bytes <strbytes@gmail.com>
2022-08-06 16:38:21 -0400

    Finish lab 6

--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -5,13 +5,13 @@ import java.io.Serializable;
 import static capers.Utils.*;
 
 /** Represents a dog that can be serialized.
- * @author TODO
+ * @author strbytes
 */
-public class Dog { // TODO
+public class Dog implements Serializable {
 
     /** Folder that dogs live in. */
-    static final File DOG_FOLDER = null; // TODO (hint: look at the `join`
-                                         //      function in Utils)
+    static final File DOG_FOLDER =
+            Utils.join(CapersRepository.CAPERS_FOLDER, "dogs");
 
     /** Age of dog. */
     private int age;
@@ -39,8 +39,9 @@ public class Dog { // TODO
      * @return Dog read from file
      */
     public static Dog fromFile(String name) {
-        // TODO (hint: look at the Utils file)
-        return null;
+        File saved = Utils.join(DOG_FOLDER, name);
+        Dog loaded = Utils.readObject(saved, Dog.class);
+        return loaded;
     }
 
     /**
@@ -56,7 +57,8 @@ public class Dog { // TODO
      * Saves a dog to a file for future use.
      */
     public void saveDog() {
-        // TODO (hint: don't forget dog names are unique)
+        File save = Utils.join(DOG_FOLDER, this.name);
+        Utils.writeObject(save, this);
     }
 
     @Override
--- lab6/capers/Main.java
+++ lab6/capers/Main.java
@@ -1,11 +1,9 @@
 package capers;
 
-import java.io.File;
-
 import static capers.Utils.*;
 
 /** Canine Capers: A Gitlet Prelude.
- * @author TODO
+ * @author strbytes
 */
 public class Main {
     /**
@@ -45,18 +43,21 @@ public class Main {
         String text;
         switch (args[0]) {
         case "story":
-            /* This call has been handled for you. The rest will be similar. */
             validateNumArgs("story", args, 2);
             text = args[1];
             CapersRepository.writeStory(text);
             break;
         case "dog":
             validateNumArgs("dog", args, 4);
-            // TODO: make a dog
+            String name = args[1];
+            String breed = args[2];
+            int age = Integer.parseInt(args[3]);
+            CapersRepository.makeDog(name, breed, age);
             break;
         case "birthday":
             validateNumArgs("birthday", args, 2);
-            // TODO: celebrate this dog's birthday
+            String birthdayDog = args[1];
+            CapersRepository.celebrateBirthday(birthdayDog);
             break;
         default:
             exitWithError(String.format("Unknown command: %s", args[0]));
