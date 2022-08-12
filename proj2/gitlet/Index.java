83e1b59201aaca7e038489d60f4e727fd6b1df13
Str Bytes <strbytes@gmail.com>
2022-08-12 17:00:10 -0400

    Add basic layout of Index class

--- proj2/gitlet/Index.java
+++ proj2/gitlet/Index.java
@@ -1,2 +1,21 @@
-package gitlet;public class Index {
+package gitlet;
+import java.util.*;
+
+public class Index implements Dumpable {
+    private List<String> files;
+    private Map<String, String> repo;
+    private Map<String, String> staged;
+    private Map<String, String> commit;
+
+    public Index() {
+        files = new ArrayList<>();
+        repo = new HashMap<>();
+        staged = new HashMap<>();
+        commit = new HashMap<>();
+    }
+
+    @Override
+    public void dump() {
+        System.out.println(files);
+    }
 }
