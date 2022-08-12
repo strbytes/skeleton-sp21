d8340055ea716291a0abd4ba1eac6bbf1d516d22
Str Bytes <strbytes@gmail.com>
2022-08-12 17:02:12 -0400

    Add Tree class (no implementation)

--- proj2/gitlet/Tree.java
+++ proj2/gitlet/Tree.java
@@ -1,2 +1,11 @@
-package gitlet;public class Tree {
+package gitlet;
+
+import java.io.Serializable;
+
+/** Stores the file tree of a Commit. */
+public class Tree implements Dumpable {
+    @Override
+    public void dump() {
+
+    }
 }
