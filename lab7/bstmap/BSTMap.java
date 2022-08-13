e334e9824153266325783f27f0244a8c77ecc293
Str Bytes <strbytes@gmail.com>
2022-08-13 13:24:03 -0400

    Initial commit of lab 7 - BSTMap skeleton
    Fill out implementation with empty methods, implement basic Node
    subclass for internal data structure. Basic implementation of put, get,
    and size methods are done but untested.

---
+++ lab7/bstmap/BSTMap.java
@@ -0,0 +1,124 @@
+package bstmap;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<K extends Comparable, V> implements Map61B<K, V> {
+
+    /* Pointer to the beginning of the BST. */
+    private Node root;
+    public int size;
+
+    /** BST data structure. */
+    private class Node {
+        private final K key;
+        private V value;
+        private Node left, right;
+
+        public Node(K k, V v) {
+            key = k;
+            value = v;
+            left = null;
+            right = null;
+        }
+    }
+
+    public BSTMap() {
+        root = null;
+        size = 0;
+    }
+
+    /** Removes all of the mappings from this map. */
+    @Override
+    public void clear() {
+        root = null;
+    }
+
+    /* Returns true if this map contains a mapping for the specified key. */
+    @Override
+    public boolean containsKey(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    /* Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
+     */
+    @Override
+    public V get(K key) {
+        return get(root, key);
+    }
+
+    /** Returns the value associated with the specified key if it is mapped,
+     * or null.
+     * @param n Node to be searched.
+     * @param key Key to search for.
+     */
+    private V get(Node n, K key) {
+        if (n == null) {
+            return null;
+        }
+        int comp = key.compareTo(n.key);
+        if (comp < 0) {
+            return get(n.left, key);
+        } else if (comp > 0) {
+            return get(n.right, key);
+        }
+        return n.value;
+    }
+
+    /* Returns the number of key-value mappings in this map. */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /* Associates the specified value with the specified key in this map. */
+    @Override
+    public void put(K key, V value) {
+        root = put(root, key, value);
+    }
+
+    private Node put(Node n, K key, V value) {
+        if (n == null) {
+            size += 1;
+            return new Node(key, value);
+        }
+        int comp = key.compareTo(n.key);
+        if (comp < 0) {
+            n.left = put(n.left, key, value);
+        } else if (comp > 0) {
+            n.right = put(n.right, key, value);
+        } else {
+            n.value = value;
+        }
+        return n;
+    }
+
+    /* Returns a Set view of the keys contained in this map. Not required for Lab 7.
+     * If you don't implement this, throw an UnsupportedOperationException. */
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    /* Removes the mapping for the specified key from this map if present.
+     * Not required for Lab 7. If you don't implement this, throw an
+     * UnsupportedOperationException. */
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    /* Removes the entry for the specified key only if it is currently mapped to
+     * the specified value. Not required for Lab 7. If you don't implement this,
+     * throw an UnsupportedOperationException.*/
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+    }
+}
