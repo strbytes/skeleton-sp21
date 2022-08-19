3b027e656b0ba9ca68ba2a6eb8766902edc4cbfb
Str Bytes <strbytes@gmail.com>
2022-08-19 08:23:19 -0400

    Overhaul Index.java
    Removed list of files, can just use the keySet of the Map files.
    Overhauled existing methods to use the new data structure and added
    methods to check if a file has been modified and to stage it if it has
    been.

--- proj2/gitlet/Index.java
+++ proj2/gitlet/Index.java
@@ -1,35 +1,50 @@
 package gitlet;
-import java.io.File;
 import java.util.*;
 
 public class Index implements Dumpable {
-    private List<String> files;
-    private Map<String, String[]> versions;
+    private final Map<String, String[]> files;
+    private final String[] versionHeaders = new String[] {"wdir", "stage", "repo"};
 
     public Index() {
-        files = new ArrayList<>();
-        versions = new HashMap<>();
+        files = new HashMap<>();
     }
 
-    public void addFile(File file) {
-        assert (!contains(file));
-        String fileName = file.getName();
-        String fileContents = Utils.readContentsAsString(file);
-        String hash = Utils.sha1(fileContents);
-        files.add(fileName);
-        versions.put(fileName, new String[] {"", "", hash});
+    public void addFile(String fileName, String hash) {
+        files.put(fileName, new String[] {hash, hash, ""});
+    }
+
+    public void updateFile(String fileName, String hash) {
+        assert contains(fileName);
+        files.get(fileName)[0] = hash;
     }
 
     public boolean contains(String fileName) {
-        return files.contains(fileName);
+        return files.containsKey(fileName);
     }
 
-    public boolean contains(File file) {
-        return files.contains(file.getName());
+    public boolean modified(String fileName) {
+        assert contains(fileName);
+        String[] versions = files.get(fileName);
+        // TODO does this work? Do I need to distinguish which is modified here?
+        return versions[0].equals(versions[1]) || versions[0].equals(versions[2]);
+    }
+
+    public void stageModified(String fileName) {
+        assert contains(fileName);
+        String[] versions = files.get(fileName);
+        versions[1] = versions[0];
     }
 
     @Override
     public void dump() {
-        System.out.println(files);
+        for (String f: files.keySet()) {
+            System.out.println(f);
+            for (int i = 0; i < 3; i++) {
+                String header = versionHeaders[i];
+                String value = files.get(f)[i];
+                System.out.println("\t" + header + "\t" + value);
+            }
+        }
+        System.out.println();
     }
 }
