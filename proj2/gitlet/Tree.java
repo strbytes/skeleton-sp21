dc88c4634130a8994b8e36eeac1760d500f9704a
Str Bytes <strbytes@gmail.com>
2022-08-19 13:24:33 -0400

    Get Tree working

--- proj2/gitlet/Tree.java
+++ proj2/gitlet/Tree.java
@@ -1,11 +1,26 @@
 package gitlet;
 
-import java.io.Serializable;
+import java.util.Map;
 
 /** Stores the file tree of a Commit. */
 public class Tree implements Dumpable {
+
+    private Map<String, String> files;
+    private String hash;
+
+    public Tree(Map<String, String> staged) {
+        files = staged;
+        hash = Utils.sha1(files.toString());
+    }
+
+    String getHash() {
+        return hash;
+    }
+
     @Override
     public void dump() {
-
+        for (String file: files.keySet()) {
+            System.out.println(file + ":\t" + files.get(file));
+        }
     }
 }
