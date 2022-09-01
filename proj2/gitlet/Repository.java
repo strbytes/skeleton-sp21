40b998347378a5356464358c5616cf0a2337d09e
Str Bytes <strbytes@gmail.com>
2022-09-01 14:34:52 -0400

    Update docstrings for Repository, some refactoring

--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -1,16 +1,15 @@
 package gitlet;
 
 import java.io.File;
+import java.util.Arrays;
 
 import static gitlet.Utils.*;
 
-// TODO: any imports you need here
-
-/** Represents a gitlet repository.
- *  TODO: It's a good idea to give a description here of what else this Class
- *  does at a high level.
+/** Represents a gitlet repository. Contains static methods for performing Gitlet
+ * commands as well as shortcuts to the files and directories used by Gitlet to
+ * maintain the repository.
  *
- *  @author TODO
+ *  @author strbytes
  */
 public class Repository {
 
@@ -22,12 +21,14 @@ public class Repository {
     public static final File OBJECTS_DIR = join(GITLET_DIR, "objects");
     /** References directory. */
     public static final File REFS_DIR = join(GITLET_DIR, "refs");
-
     /** The index file to keep track of the working directory. */
     public static final File INDEX = join(GITLET_DIR, "index");
     /** HEAD pointer. Keeps track of current position in the commit tree. */
     public static final File HEAD = join(GITLET_DIR, "head");
 
+    /** Initializes a new .gitlet directory and the files and folders needed to maintain it.
+     * Creates an initial commit and master branch.
+     */
     public static void init() {
         if (GITLET_DIR.exists()) {
             Utils.message("A Gitlet version-control system already exists in the current directory.");
@@ -39,14 +40,16 @@ public class Repository {
         Commit initialCommit = new Commit();
         String initialCommitHash = initialCommit.getHash();
         writeObject(initialCommitHash, initialCommit);
-        String branch = newBranch("master", initialCommitHash);
+        String branch = "master";
+        newBranch(branch, initialCommitHash);
         Utils.createFile(HEAD);
-        // TODO -- update other methods that use HEAD to get the Branch from this
         Utils.writeContents(HEAD, branch);
         Index index = new Index();
         Utils.writeObject(INDEX, index);
     }
 
+    /** Utility method to inspect object files created by Gitlet. Accessed by calling
+     * Gitlet with the type and hash of the object to be read. */
     public static void catFile(String type, String hash) {
         String dirName = hash.substring(0, 2);
         String fileName = hash.substring(2);
@@ -73,18 +76,28 @@ public class Repository {
         }
     }
 
+    /** Utility method that displays the contents of the Index file. */
     public static void lsFiles() {
         Index index = Utils.readObject(INDEX, Index.class);
         System.out.println(index);
     }
 
-    public static String newBranch(String name, String commit) {
+    /** Create a new branch.
+     *
+     * @param name Name of the branch to be created.
+     * @param commit Hash of the commit to use as the head commit for this branch.
+     */
+    public static void newBranch(String name, String commit) {
+        // TODO check for duplicate names.
         File branch = join(REFS_DIR, name);
         Utils.createFile(branch);
         Utils.writeContents(branch, commit);
-        return name;
     }
 
+    /** Add a file to the staging area.
+     *
+     * @param fileName Name of the file to be added.
+     */
     public static void add(String fileName) {
         File file = join(CWD, fileName);
         if (!file.exists()) {
@@ -109,7 +122,12 @@ public class Repository {
         }
     }
 
+    /** Create a new commit with all the changed files in the staging area.
+     *
+     * @param message Message associated with the commit.
+     */
     public static void commit(String message) {
+        // TODO check that files have changed.
         String branch = Utils.readContentsAsString(HEAD);
         String prevCommit = getBranch(branch);
         Index index = Utils.readObject(INDEX, Index.class);
@@ -139,6 +157,7 @@ public class Repository {
         Utils.writeObject(INDEX, index);
     }
 
+    /** Get the commit hash from the branch associated with branchName. */
     public static String getBranch(String branchName) {
         File branch = Utils.join(REFS_DIR, branchName);
         return readContentsAsString(branch);
@@ -159,6 +178,9 @@ public class Repository {
         }
     }
 
+    /** Check out a branch by name.
+     *
+     * Called with format 'gitlet checkout <branchName>'. */
     public static void checkoutBranch(String branchName) {
         // TODO -- untested, cannot create new branches yet
         if (!Arrays.asList(REFS_DIR.list()).contains(branchName)) {
@@ -174,6 +196,9 @@ public class Repository {
         }
     }
 
+    /** Check out a file by name.
+     *
+     * Called with format 'gitlet checkout -- <fileName>'. */
     public static void checkoutFile(String fileName) {
         Index index = Utils.readObject(INDEX, Index.class);
         String fileHash = index.getVersions(fileName)[2];
@@ -186,7 +211,11 @@ public class Repository {
         Utils.writeContents(file, fileContents);
     }
 
+    /** Check out a file from a previous commit.
+     *
+     * Called with format 'gitlet checkout <hash> <fileName>'. */
     public static void checkoutFileFromCommit(String hash, String fileName) {
+//        hash = getFullHash(hash);
         Commit commit = readObject(hash, Commit.class);
         String treeHash = commit.getTree();
         Tree tree = readObject(treeHash, Tree.class);
